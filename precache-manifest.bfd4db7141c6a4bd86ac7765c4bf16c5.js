5fcd67855450efe6f333cc43ea02b34cb6101d67
Aodhagan Murphy <aodhagan.murphy@magellanrobotech.com>
2020-05-24 18:43:27 +0100

    Updates

--- precache-manifest.bfd4db7141c6a4bd86ac7765c4bf16c5.js
+++ precache-manifest.bfd4db7141c6a4bd86ac7765c4bf16c5.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "01d62e47fdbae0c7af313646f9575505",
+    "revision": "4cc76a049cafdc5b5118a26e91a72e6f",
     "url": "/index.html"
   },
   {
-    "revision": "8b45115b9e13778d772a",
+    "revision": "172b4c1e798211966bc9",
     "url": "/static/css/main.f2218413.chunk.css"
   },
   {
-    "revision": "bfbb1bc1c0fd0c0f53b9",
-    "url": "/static/js/2.45a2b8dc.chunk.js"
+    "revision": "49e3197167a7e22aff93",
+    "url": "/static/js/2.16d981be.chunk.js"
   },
   {
     "revision": "467b8636c6f77c841dd8fbbfe186ca3a",
-    "url": "/static/js/2.45a2b8dc.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.16d981be.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8b45115b9e13778d772a",
-    "url": "/static/js/main.578b8c38.chunk.js"
+    "revision": "172b4c1e798211966bc9",
+    "url": "/static/js/main.488de457.chunk.js"
   },
   {
     "revision": "441589a6dca4d2e4936c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.25c4a9d1d40dcf6ca9ddf3437b35eee7.js"
+  "/precache-manifest.bfd4db7141c6a4bd86ac7765c4bf16c5.js"
 );
 
 self.addEventListener('message', (event) => {
