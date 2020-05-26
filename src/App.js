e3727782869e57ae890f10fe6e451d0f2f6179cf
Aodhagan Murphy <aodhagan.murphy@magellanrobotech.com>
2020-05-26 10:45:37 +0100

    fixed tabs on App.js

--- src/App.js
+++ src/App.js
@@ -3,52 +3,55 @@ import { HashRouter as Router, Route } from "react-router-dom";
 
 import "./App.scss";
 import { SiteNavbar, Footer, Loader } from "./components/widgets";
-import Page from './components/page';
+import Page from "./components/page";
 import { contentService } from "./services";
 
 export default class App extends Component {
-  state = {
-    loading: true
-  }
+	state = {
+		loading: true
+	};
 
 	constructor(props) {
-    super(props);
-    this.contentService = contentService;
-  }
+		super(props);
+		this.contentService = contentService;
+	}
 
 	componentDidMount() {
-    this.contentService.getAllPagesContent()
-      .then(pages => {
-        const links = pages.reduce((acc, page) => {
-            const { title, route } = page.fields;
-            if (route === '/') {
-              return [{ name: 'Home', route }, ...acc];
-            }
-            return [...acc, { name: title, route }];
-        }, []);
+		this.contentService.getAllPagesContent().then(pages => {
+			const links = pages.reduce((acc, page) => {
+				const { title, route } = page.fields;
+				if (route === "/") {
+					return [{ name: "Home", route }, ...acc];
+				}
+				return [...acc, { name: title, route }];
+			}, []);
 
-        this.setState({
-          loading: false,
-          links,
-          pages
-        });
-      });
-  }
+			this.setState({
+				loading: false,
+				links,
+				pages
+			});
+		});
+	}
 
 	render() {
-    const { loading, links, pages } = this.state;
-    if (loading) {
-      return <Loader />;
-    }
+		const { loading, links, pages } = this.state;
+		if (loading) {
+			return <Loader />;
+		}
 
 		return (
 			<Router>
 				<SiteNavbar links={links} />
-        {pages.map((page, index) => (
-          <Route key={index} path={page.fields.route} exact={page.fields.route === '/'}>
-            <Page content={page} />
-          </Route>
-        ))}
+				{pages.map((page, index) => (
+					<Route
+						key={index}
+						path={page.fields.route}
+						exact={page.fields.route === "/"}
+					>
+						<Page content={page} />
+					</Route>
+				))}
 				<Footer />
 			</Router>
 		);
